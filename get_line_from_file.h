20d7a68855cf3c5c563b25e9972c24f1a954be7f
Amit Choudhary <amitchoudhary0523@gmail.com>
2025-05-09 18:21:20 +0530

    Updated get_line_from_file.h
    Updated get_line_from_file.h

--- get_line_from_file.h
+++ get_line_from_file.h
@@ -31,7 +31,8 @@
  * char *get_line_from_file(int fd, int *error_num):
  *
  * Function get_line_from_file() is an alternate implementation of C library's
- * getline() function.
+ * getline() function and this function is simpler to use than getline().
+ *
  *
  * Function get_line_from_file() returns a line from the file represented by fd.
  *
